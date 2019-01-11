d9c7a5e028e8e89b726c19d40e97b078d92471ec
david davila <david.davila@cdk.com>
2019-01-11 15:10:15 -0800

    Re-worked the entire app now it's hot reloading and showing a simple inventory

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,53 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import "./styles.scss";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import products from './products';
+import { Card, Icon, Image } from 'semantic-ui-react';
+import { observable, action } from 'mobx';
+import { observer } from 'mobx-react';
 
-class App extends React.Component<{}> {
-  render() {
-    return <div>Start writing your app here.</div>;
+class Products {
+  @observable p = products;
+
+  @action
+  getProduct() {
+    const keys = Object.keys(this.p);
+    return products[keys[Math.floor((Math.random() * keys.length))]];
   }
 }
 
+@observer
+class App extends React.Component<{}> {
+  data = new Products().getProduct();
+  render() {
+    return (
+      <Card>
+        <Image src={this.data.image} />
+        <Card.Content>
+          <Card.Header>{this.data.name}</Card.Header>
+          <Card.Meta>
+            <span className='date'></span>
+          </Card.Meta>
+          <Card.Description>{this.data.description}</Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+        <a>
+            <Icon name='id badge' />
+            {this.data.reviewCount}
+          </a>
+        </Card.Content>
+        <Card.Content extra>
+          <a>
+            <Icon name='star' />
+            {this.data.rating}
+          </a>
+        </Card.Content>
+        </Card>
+    );
+  }
+}
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,16 @@
 module.exports = {
-    devtool: 'inline-source-map',
-    entry: './src/app.tsx',
-    output: {
-      path: __dirname + '/public',
-      filename: 'build/app.js',
-      publicPath: '/public'
-    },
-    resolve: {
-      extensions: ['.ts', '.tsx', '.js']
-    },
-    module: {
-      rules: [
-        { test: /\.tsx?$/, loader: 'ts-loader' },
-        {
-          test:/\.(s*)css$/,
-          use:['style-loader','css-loader', 'sass-loader']
-        }
-      ]
-    }
+  devtool: 'inline-source-map',
+  entry: './src/app.tsx',
+  output: {
+    path: __dirname + '/public',
+    filename: 'build/app.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: 'ts-loader' }
+    ]
   }
+}
