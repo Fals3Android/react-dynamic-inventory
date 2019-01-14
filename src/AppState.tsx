85426984af4f8b6a2a28acbb57ba812ebbcee7ed
david davila <david.davila@cdk.com>
2019-01-14 09:31:09 -0800

    Modified observable class to it's own file

---
+++ src/AppState.tsx
@@ -0,0 +1,42 @@
+import products from './products';
+import { observable, action, computed } from 'mobx';
+
+interface ProductListings {
+  name: string,
+  description: string,
+  image: string,
+  price: string,
+  rating: string,
+  reviewCount: string
+}
+
+class AppState {
+    @observable p = products;
+  
+    @observable currentProduct: ProductListings = {
+      name: "",
+      description: "",
+      image: "",
+      price: "",
+      rating: "",
+      reviewCount: ""
+    };
+  
+    @action
+    getProduct() {
+      const keys = Object.keys(this.p);
+      return this.p[keys[Math.floor((Math.random() * keys.length))]];
+    }
+  
+    @action
+    setProduct(product) {
+      this.currentProduct = product;
+    }
+  
+    @computed
+    get hasFiveStarRating() {
+      return this.currentProduct.rating === "5";
+    }
+  }
+
+  export const appState = new AppState();
--- src/app.tsx
+++ src/app.tsx
@@ -1,44 +1,13 @@
 import * as React from 'react';
-import products from './products';
 import { Card, Icon, Image } from 'semantic-ui-react';
-import { observable, action, computed } from 'mobx';
+import { appState } from './AppState';
 import { observer } from 'mobx-react';
-
-class Products {
-  @observable p = products;
-
-  @observable currentProduct = {
-    name: "",
-    description: "",
-    image: "",
-    price: "",
-    rating: "",
-    reviewCount: ""
-  };
-
-  @action
-  getProduct() {
-    const keys = Object.keys(this.p);
-    return this.p[keys[Math.floor((Math.random() * keys.length))]];
-  }
-
-  @action
-  setProduct(product) {
-    this.currentProduct = product;
-  }
-
-  @computed
-  get hasFiveStarRating() {
-    return parseInt(this.currentProduct.rating) === 5;
-  }
-}
 
 @observer
 class App extends React.Component<{}> {
-  data = new Products();
   render() {
-    const product = this.data.getProduct();
-    this.data.setProduct(product);
+    const product = appState.getProduct();
+    appState.setProduct(product);
 
     return (
       <Card>
@@ -62,7 +31,7 @@ class App extends React.Component<{}> {
             {product.rating}
           </a>
           { 
-            this.data.hasFiveStarRating 
+            appState.hasFiveStarRating 
             && <p>Preferred product !!!</p>
           }
         </Card.Content>
